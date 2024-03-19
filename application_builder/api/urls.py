c05ea75c9835a17bfec17c365e074b5908047401
Мусатова Татьяна <MusatovaTF@yandex.ru>
2024-03-19 10:50:42 +0300

    Черновик applications

--- application_builder/api/urls.py
+++ application_builder/api/urls.py
@@ -3,11 +3,13 @@ from rest_framework.routers import DefaultRouter
 
 from api.industries.views import IndustryViewSet
 from api.citizenships.views import CitizenshipViewSet
+from api.professions.views import ProfessionSkillViewSet
 
 app_name = 'api'
 VERSION = 'v2'
 
 router = DefaultRouter()
+router.register('applications', IndustryViewSet, basename='application')
 router.register('industries', IndustryViewSet, basename='industry')
 router.register('citizenships', CitizenshipViewSet, basename='citizenship')
 
