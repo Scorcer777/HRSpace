749e6e8f3dcfcc59899373bf74d0f2bbd32ec494
Кирилл Новоселов <fornka2006@yandex.ru>
2024-03-20 17:44:14 +0300

    add api/applications, change fixtures according to the new models 2

--- application_builder/api/urls.py
+++ application_builder/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.industries.views import IndustryViewSet
+from api.applications.views import ApplicationCreateView
 
 app_name = 'api'
 VERSION = 'v2'
@@ -11,5 +12,8 @@ router.register('industries', IndustryViewSet, basename='industry')
 
 
 urlpatterns = [
+    path('applications/create/',
+         ApplicationCreateView.as_view(),
+         name='applications-create'),
     path(f'{VERSION}/', include(router.urls)),
 ]
