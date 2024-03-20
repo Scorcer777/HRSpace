2d7e9d070ef3961ffce9d868cb7fd01ae70c8d58
Мусатова Татьяна <MusatovaTF@yandex.ru>
2024-03-20 10:39:38 +0300

    Исправленна админка

--- application_builder/apps/cities/admin.py
+++ application_builder/apps/cities/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import City
+from .models import City, Region
 
 
 @admin.register(City)
 class CityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'region')
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
     list_display = ('id', 'name',)
