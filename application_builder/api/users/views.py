1c533a31e83f3688fc3db577e75b842cbc4e76c2
Наталья Сорокина <natalya_40ina@mail.ru>
2024-03-22 00:45:35 +0600

    fixed auth response

--- application_builder/api/users/views.py
+++ application_builder/api/users/views.py
@@ -2,14 +2,24 @@ from rest_framework.authtoken.models import Token
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 from .serializers import LoginSerializer
 from apps.users.models import CustomUser
 
 
 class CustomAuthToken(GenericAPIView):
+
     serializer_class = LoginSerializer
 
+    @swagger_auto_schema(
+        request_body=LoginSerializer,
+        responses={
+            201: openapi.Response(description='Successful login', schema=openapi.Schema(type='object', properties={'auth_token': openapi.Schema(type='string', description='JWT token for authentication')})),
+            401: openapi.Response(description='Unable to log in with provided credentials', schema=openapi.Schema(type='object', properties={'detail': openapi.Schema(type='string', description='Error message')}))
+        }
+    )
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
